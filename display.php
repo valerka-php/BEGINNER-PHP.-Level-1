d309c2f13a199ce8d3a8b19c8e5e32d3469d2dac
Valerii <nyshnui@gmail.com>
2021-03-29 19:12:24 +0300

    made 29.03.2021

---
+++ display.php
@@ -0,0 +1,25 @@
+<?php
+
+$a = 5;
+$b = 'some words';
+$b1 ='make a phrase';
+$c = 2.45;
+$bool = true;
+$null = null;
+
+$d = $a + $c;
+$d1 = $b . $b1;
+$d2 = $a - $c;
+
+echo $a;
+echo $b;
+echo $c;
+
+//var_dump($a);
+//var_dump($b);
+//var_dump($c);
+var_dump($d1);
+var_dump($null);
+var_dump($d);
+var_dump($bool);
+vad_dump($d2);
