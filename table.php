bf3747a0959e7bdda9b21e8cb9ca4fa72d9ffd9f
Valerii <nyshnui@gmail.com>
2021-04-13 23:22:26 +0300

    add table

--- table.php
+++ table.php
@@ -1,40 +1,33 @@
 <?php
-// first table
-    $low1 = 1;
-    $high1 =5;
 
-for ($i1 = $low1; $i1 <= $high1; $i1++) {
-    for ($j1 = $low1; $j1 <=$high1; $j1++){
-        $array1 [$i1][$j1] = $i1*$j1;
+for ($i = 1; $i <= 5; $i++) {
+    for ($j = 1; $j <=10; $j++){
+        $array [$i][$j] = $i*$j;
     }
 }
-
-echo '<table border= "1" ';
-foreach ($array1 as $key1 => $item1) {
-    echo '<tr>';
-    foreach ($item1 as $key2 => $value1) {
-        echo "<td> {$key2}*{$key1}= $value1  </td> ";
-
-        }
-    echo '</tr>';
-    }
-
-// second table
-$low2 = 6;
-$high2 =10;
-
-for ($i2 = $low2; $i2 <= $high2; $i2++) {
-    for ($j2 = $low2; $j2 <=$high2; $j2++){
+for ($i2 = 6; $i2 <= 10; $i2++) {
+    for ($j2 = 1; $j2 <=10; $j2++){
         $array2 [$i2][$j2] = $i2*$j2;
     }
 }
 
-echo '<table border= "1" ';
-foreach ($array2 as $key3 => $item2) {
-    echo '<tr>';
-    foreach ($item2 as $key4 => $value2) {
-        echo "<td> {$key4}*{$key3}= $value2  </td> ";
 
+echo '<table border= "3" ';
+echo '<tr bordercolor = "#00000" align="center" >';
+foreach ($array as $key1 => $item) {
+    echo '<td>' ;
+    foreach ($item as $key2 => $value) {
+        echo "{$key1} x {$key2} = $value <br>" ;
     }
-    echo '</tr>';
+     echo '</td>';
 }
+echo '</tr>';
+echo '<tr>';
+foreach ($array2 as $key3 => $item2) {
+    echo "<td>" ;
+    foreach ($item2 as $key4 => $value2) {
+        echo "{$key3} x {$key4} = $value2 <br>" ;
+    }
+    echo "</td>";
+}
+echo '</tr>';
